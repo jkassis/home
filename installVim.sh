7b0228bcb2029cfe4dc54f9ea0d09609db68c340
Jeremy Kassis <jkassis@gmail.com>
2018-10-25 15:27:31 -0700

    neovim nv link made using which

--- installVim.sh
+++ installVim.sh
@@ -2,8 +2,8 @@
 mkdir ~/.config
 ln -sf ~/Home/.config/nvim ~/.config/nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
-sudo ln -s /usr/local/bin/nvim /usr/local/bin/nv
+nvim +PlugInstall +qall
+sudo ln -s $(which nvim) $(which nvim)/../nv 
 pip2 install neovim --upgrade
 pip3 install neovim --upgrade
 
