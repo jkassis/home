19bbf1f199de933b8e9531d3369fd1a1437f992c
Jeremy Kassis <jkassis@gmail.com>
2018-10-25 15:38:07 -0700

    fix neovim linking

--- installVim.sh
+++ installVim.sh
@@ -3,7 +3,8 @@ mkdir ~/.config
 ln -sf ~/Home/.config/nvim ~/.config/nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
-sudo ln -s $(which nvim) $(which nvim)/../nv 
+VIMDIR=$(dirname $(which nvim))
+sudo ln -s $VIMDIR/nvim $VIMDIR/nv
 pip2 install neovim --upgrade
 pip3 install neovim --upgrade
 
