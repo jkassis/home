8daf7d88d6651b1c65da3c84d527ce3687713acf
Jeremy Kassis <jkassis@gmail.com>
2018-10-25 16:43:58 -0700

    don\'t remove .zshrc in install.sh

--- install.sh
+++ install.sh
@@ -114,7 +114,6 @@ curl "https://raw.githubusercontent.com/nathanbuchar/atom-one-dark-terminal/mast
 brew install zsh
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm ~/.zshrc
 
 # shell : directory environments
 brew install direnv
