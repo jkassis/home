da45d7457147c76be5f5855897517b79650fed4e
Jeremy Kassis <jkassis@gmail.com>
2018-10-25 16:58:49 -0700

    fix zsh install dir

--- installZsh.sh
+++ installZsh.sh
@@ -6,5 +6,5 @@ mkdir ~/.zsh
 touch ~/.zshrc.local
 
 # install autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
