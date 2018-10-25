88266918d2440faa7dd280acd6e29bd15a3788aa
Jeremy Kassis <jkassis@gmail.com>
2018-10-25 15:57:07 -0700

    force link creation

--- installLinks.bash
+++ installLinks.bash
@@ -1,9 +1,9 @@
 # Link everything
-ln -s ~/Home/.zshrc ~/.zshrc
-ln -s ~/Home/.gitconfig ~/.gitconfig
-ln -s ~/Home/.gitignore_global ~/.gitignore_global
-ln -s ~/Home/.ideavimrc ~/.ideavimrc
-ln -s ~/Home/.tmux.conf ~/.tmux.conf
-ln -s ~/Home/.vim ~/.vim
-ln -s ~/Home/.zshrc ~/.zshrc
-ln -s ~/Home/bin ~/bin
+ln -sf ~/Home/.zshrc ~/.zshrc
+ln -sf ~/Home/.gitconfig ~/.gitconfig
+ln -sf ~/Home/.gitignore_global ~/.gitignore_global
+ln -sf ~/Home/.ideavimrc ~/.ideavimrc
+ln -sf ~/Home/.tmux.conf ~/.tmux.conf
+ln -sf ~/Home/.vim ~/.vim
+ln -sf ~/Home/.zshrc ~/.zshrc
+ln -sf ~/Home/bin ~/bin
