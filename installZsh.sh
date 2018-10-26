9d073793747efd5902d28436f05753f8b53a158b
Jeremy Kassis <jkassis@gmail.com>
2018-10-25 17:07:49 -0700

    oh-my-zsh install creates the ~/.oh-my-zsh dir

--- installZsh.sh
+++ installZsh.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # make directories
-mkdir ~/.oh-my-zsh
 mkdir ~/.zsh
 touch ~/.zshrc.local
 
 # install autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
+
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
