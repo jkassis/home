077b280f9ddd2d2924a153bfe645521ddc8c6329
Jeremy Kassis <jkassis@gmail.com>
2018-10-26 17:03:57 -0700

    fix links scripts

---
+++ installLinks.sh
@@ -0,0 +1,9 @@
+# Link everything
+ln -sf ~/Home/.zshrc ~/.zshrc
+ln -sf ~/Home/.gitconfig ~/.gitconfig
+ln -sf ~/Home/.gitignore_global ~/.gitignore_global
+ln -sf ~/Home/.ideavimrc ~/.ideavimrc
+ln -sf ~/Home/.tmux.conf ~/.tmux.conf
+ln -sf ~/Home/.vim ~/.vim
+ln -sf ~/Home/.zshrc ~/.zshrc
+ln -sf ~/Home/bin ~/bin
