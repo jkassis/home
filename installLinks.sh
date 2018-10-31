a7008f4500d428f98d45e6d484ed0e5a638d931a
Jeremy Kassis <jkassis@gmail.com>
2018-10-31 12:08:17 -0700

    idempotency for installLinks.sh

--- installLinks.sh
+++ installLinks.sh
@@ -1,11 +1,12 @@
 # Link everything
-ln -sf ~/Home/.zshrc ~/.zshrc
-ln -sf ~/Home/.gitconfig ~/.gitconfig
-ln -sf ~/Home/.gitignore_global ~/.gitignore_global
-ln -sf ~/Home/.ideavimrc ~/.ideavimrc
-ln -sf ~/Home/.tmux.conf ~/.tmux.conf
-ln -sf ~/Home/.vim ~/.vim
-ln -sf ~/Home/.zshrc ~/.zshrc
-ln -sf ~/Home/bin ~/bin
+ln -sf ~/Home/.zshrc ~
+ln -sf ~/Home/.gitconfig ~
+ln -sf ~/Home/.gitignore_global ~
+ln -sf ~/Home/.ideavimrc ~
+ln -sf ~/Home/.tmux.conf ~
+ln -sf ~/Home/.vim ~
+ln -sf ~/Home/.zshrc ~
+ln -sf ~/Home/bin ~
+ln -sf $(which nvim) /usr/local/bin/nv
 
 alias kcDashToken=~/Home/bin/kcDashToken.bash
