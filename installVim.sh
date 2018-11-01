2fab9fe36aa10b8bf5031dc96aa8b42682656608
Jeremy Kassis <jkassis@gmail.com>
2018-11-01 10:58:57 -0700

    Make installVim.sh idempotent

--- installVim.sh
+++ installVim.sh
@@ -1,6 +1,6 @@
 # editors : nvim
 mkdir ~/.config
-ln -sf ~/Home/.config/nvim ~/.config/nvim
+ln -sf ~/Home/.config/nvim ~/.config
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
 VIMDIR=$(dirname $(which nvim))
