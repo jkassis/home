9212d4e32aba9c73cc804fdafcf83d8b80abdf38
Jeremy Kassis <jkassis@gmail.com>
2018-11-14 20:39:07 -0800

    cleanup installVim.sh

--- installVim.sh
+++ installVim.sh
@@ -1,11 +1,18 @@
-# editors : nvim
+# setup the config dir
 mkdir ~/.config
 ln -sf ~/Home/.config/nvim ~/.config
+
+# install vim-plug and install plugins
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
+
+# set the env (for what?)
 VIMDIR=$(dirname $(which nvim))
+
+# install plugin providers
 pip install neovim --upgrade
 pip3 install neovim --upgrade
+npm install -g neovim
 
 # Add link to nv
 if [ "$EUID" -ne 0 ]
