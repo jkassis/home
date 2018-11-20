01fed6efc08409bcc594f46dddd7b0e744dcf7b2
Jeremy Kassis <jkassis@gmail.com>
2018-11-20 15:26:54 -0800

    add installCentos.bash

---
+++ bin/installCentos.sh
@@ -0,0 +1,17 @@
+$!/usr/bin/env bash
+
+# Install developer tools
+yum install -y rsync bash zsh git ssh openssh-server openssh-clients the_silver_searcher python python3 fzf ctags python34-neovim less curl wget vim python-pip python34-pip php-xdebug golang gcc make perl-Error coreutils autoload node npm
+
+# Fix python links
+ln -s /usr/bin/python /usr/local/bin/python
+ln -s /usr/bin/python3 /usr/local/bin/python3
+
+cd ~
+
+# Install everything else
+Home/bin/installZsh.sh || true
+Home/bin/installVim.sh || true
+Home/bin/installLinks.sh || true
+Home/bin/installLemonade.sh || true
+
