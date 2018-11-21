9dedea9444cd603dfd9da84f49ad82dfca726536
Jeremy Kassis <jkassis@mz.com>
2018-11-20 18:04:58 -0800

    fix fd-find install on centos

--- bin/installCentos.sh
+++ bin/installCentos.sh
@@ -1,7 +1,10 @@
 $!/usr/bin/env bash
 
 # Install developer tools
-yum install -y rsync bash zsh git ssh openssh-server openssh-clients the_silver_searcher python python3 fzf ctags python34-neovim less curl wget vim python-pip python34-pip php-xdebug golang gcc make perl-Error coreutils autoload node npm fd fd-find
+yum install -y rsync bash zsh git ssh openssh-server openssh-clients the_silver_searcher python python3 fzf ctags python34-neovim less curl wget vim python-pip python34-pip php-xdebug golang gcc make perl-Error coreutils autoload node npm rust-toolset-7-cargo
+
+# fd
+cargo install fd-find
 
 # Fix python links
 ln -s /usr/bin/python /usr/local/bin/python
