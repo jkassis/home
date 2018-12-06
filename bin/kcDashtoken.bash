0d8404fb252affe1c90b578bcaf63cd609e6cda2
Jeremy Kassis <jkassis@mz.com>
2018-12-06 10:53:19 -0800

    take first token in kcDashtoken.bash

--- bin/kcDashtoken.bash
+++ bin/kcDashtoken.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # " kubectl -n kube-system get secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}') -o jsonpath="{.data.token}" | base64 --decode | pbcopy
-kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep tiller-token | awk '{print $1}') | grep ^token | awk '{print $2}' | pbcopy
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep tiller-token | head -n 1 | awk '{print $1}') | grep ^token | awk '{print $2}' | pbcopy
