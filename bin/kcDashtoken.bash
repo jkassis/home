ca54718aafc1c88d59bdf74c4288f9c629fdd683
Jeremy Kassis <jkassis@gmail.com>
2019-01-20 05:23:22 -0800

    get token using tiller_namespace

--- bin/kcDashtoken.bash
+++ bin/kcDashtoken.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# " kubectl -n kube-system get secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}') -o jsonpath="{.data.token}" | base64 --decode | pbcopy
-kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep tiller-token | head -n 1 | awk '{print $1}') | grep ^token | awk '{print $2}' | pbcopy
+# " kubectl -n $TILLER_NAMESPACE -system get secret $(kubectl -n $TILLER_NAMESPACE -system get secret | grep admin-user | awk '{print $1}') -o jsonpath="{.data.token}" | base64 --decode | pbcopy
+kubectl -n $TILLER_NAMESPACE describe secret $(kubectl -n $TILLER_NAMESPACE get secret | grep tiller-token | head -n 1 | awk '{print $1}') | grep ^token | awk '{print $2}' | pbcopy
