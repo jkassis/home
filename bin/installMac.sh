7dfd422d27ee86c02c7724c0faccb700aff5e176
Jeremy Kassis <jkassis@mz.com>
2019-01-30 14:48:17 -0800

    install mac cleanup

--- bin/installMac.sh
+++ bin/installMac.sh
@@ -8,22 +8,21 @@ brew tap caskroom/versions
 brew cask install google-chrome
 brew cask install firefoxdeveloperedition
 
-# desktop productivity
-#brew cask install alfred
-# TODO: alfred/spotlight - can we automate setting the hotkeys?
-
-# window management
-brew cask install spectacle
-
-# monitor management
+# display and window management 
+# brew cask install spectacle
+brew cask install magnet
 brew cask install duet
 
-# dock setting
+# osx : dock setting
 # "Set Dock to auto-hide and remove the auto-hiding delay? (y/n)"
 #defaults write com.apple.dock autohide-delay -float 0
 #defaults write com.apple.dock autohide-time-modifier -float 0
 
-# special key access
+# osx : disable guest account
+defaults write /Library/Preferences/com.apple.AppleFileServer guestAccess -bool NO
+defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess -bool NO
+
+# osx : special key access
 # "Disabling press-and-hold for special keys in favor of key repeat"
 #defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 # TODO: look into https://gist.github.com/brandonb927/3195465 and related for more tweaks
@@ -42,12 +41,9 @@ brew cask install keeper
 brew cask install lastpass
 # TODO: can we automate 1password setup/config?
 
-# communication
+# messaging
 brew cask install slack
 # TODO: slack - auto add my specific servers?
-
-# virtual machine managers
-brew cask install virtualbox
 
 # languages : java
 brew cask install java8
@@ -56,6 +52,7 @@ brew cask install java8
 #brew install node
 #npm install -g jasmine-core
 #npm install -g karma karma-jasmine karma-coverage karma-webpack karma-sourcemap-loader karma-chrome-launcher
+#npm install -g webpack
 
 ## languages: python
 brew install python@2
@@ -65,8 +62,6 @@ pip2 install virtualenv virtualenvwrapper flake8
 #brew install go --cross-compile-common
 #mkdir -p "$HOME/Code/go"
 
-## build system
-#npm install -g webpack
 
 # editors : atom
 # brew cask install atom
@@ -84,34 +79,51 @@ brew cask install sublime-text3
 # editors : emacs
 brew cask install emacs
 
-# vim
+# editors : vim
 brew install python python3 nvim fzf the_silver_searcher ctags fd
 ./installVim.sh
 
-# oni
-# brew cask install oni
-# defaults write com.extropy.oni ApplePressAndHoldEnabled -bool false
-
-# ides
+# ides : xcode
 xcode-select --install
 sudo xcodebuild -license accept # agree to xcode license
 
-# vs code
+# ides : vscode
 brew cask install visual-studio-code
 mkdir -p ~/Library/Application\ Support/Code/User ; rm ~/Library/Application\ Support/Code/User/settings.json ; ln ~/home/.vs.code.settings.json ~/Library/Application\ Support/Code/User/settings.json
 
-# hosting : heroku
+# cloud 
 brew install heroku-toolbelt
+brew install amazoncli
 
-# hosting : docker
+# containers : vm hosts
+brew cask install virtualbox
+
+# container : hosts
 brew cask install docker
+
+# container : orchestration : kubernetes
+brew install kubernetes-helm terraform cfssl
+brew install jsonnet
+
+# format utils : yaml json
+pip3 install yq
+go get github.com/brancz/gojsontoyaml
 brew install jq
 
-# file transfer
+# protocol clients : http
 brew install httpie
+# protocol clients : ws
+go get -u github.com/hashrocket/ws
+# protocol client : TCP/UDP
+brew install ncat
+# protocol clients : dns
+brew install dig
+
+
+# file sharing
 brew cask install dropbox
 
-# terminal
+# terminal emulators
 brew cask install iterm2
 # curl "https://raw.githubusercontent.com/nathanbuchar/atom-one-dark-terminal/master/scheme/iterm/One%20Dark.itermcolors" > ~/Downloads/OneDark.itermcolors
 
@@ -121,50 +133,34 @@ brew install zsh
 # shell : directory environments
 brew install direnv
 
-# elasticsearch
+# db : elasticsearch
 brew install elasticsearch@5.6
 brew services start elasticsearch@5.6
 
-# databases
-# brew install neo4j
+# db : neo4j
+brew install cockroachdb
+brew install neo4j
 # brew install postgresql
 # brew cask install postgres
-# brew cask install pgadmin4
+brew cask install pgadmin4
 # brew services start postgresql
 
-# kubernetes
-brew install kubernetes-helm terraform cfssl
-pip3 install yq
-brew install jsonnet
-go get github.com/brancz/gojsontoyaml
-
-
-# sourcetree
+# source control
 brew cask install sourcetree
-
-# beyond compare
 brew cask install beyond-compare
-
-# ydiff
 brew install ydiff
 
-# font setup
+# fonts
 brew tap caskroom/fonts
 brew cask install font-fira-code font-hack-nerd-font
 
-# disable guest account
-defaults write /Library/Preferences/com.apple.AppleFileServer guestAccess -bool NO
-defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess -bool NO
-
 # network monitoring
 brew install little-snitch
+
+# sbc tools
+brew cask install balenaetcher kekka angry-ip-scanner
 
 echo "Post installation tips:"
 echo "- Set fira-code font in iterm2"
 echo "- Set itermcolor scheme (file is OneDark.itermcolors)"
 
-# tools for single board computers
-brew cask install balenaetcher kekka angry-ip-scanner
-
-# tools for amazon aws
-brew install amazoncli
