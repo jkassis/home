96ff4b36d1006b1a4f5fb1b885231eb21a08e666
Jeremy Kassis <jkassis@gmail.com>
2022-07-05 13:19:11 -0700

    install gitall

--- bin/installMac.sh
+++ bin/installMac.sh
@@ -32,6 +32,10 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
+# gitutils
+brew tap jkassis/tap
+brew install gitall
+
 # gags
 brew install sl
 
