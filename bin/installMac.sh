c7c06cb8c739c0c5823fa9018e0bcf730facc1a2
Jeremy Kassis <jkassis@gmail.com>
2022-07-08 10:03:04 -0700

    move to homebrew-keg

--- bin/installMac.sh
+++ bin/installMac.sh
@@ -33,7 +33,7 @@ defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
 # gitutils
-brew tap jkassis/tap
+brew tap jkassis/keg
 brew install gitall
 
 # gags
