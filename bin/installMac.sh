5fd5fdccb3528689798244345107f5468254e358
Jeremy Kassis <jkassis@gmail.com>
2023-04-19 17:13:51 -0700

    minor updates

--- bin/installMac.sh
+++ bin/installMac.sh
@@ -8,7 +8,7 @@ brew tap homebrew/cask-versions
 brew install --cask google-chrome firefox-developer-edition
 brew install wget
 
-# display and window management 
+# display and window management
 # brew install --cask spectacle
 # brew install --cask magnet pbcopy
 # brew install --cask duet
@@ -85,7 +85,7 @@ brew install hammerspoon
 #pip2 install virtualenv virtualenvwrapper flake8
 
 ## languages: go
-brew install golang 
+brew install golang
 # brew install goreleaser
 mkdir -p "$HOME/Code/go"
 # go install github.com/karalabe/xgo@latest
@@ -121,16 +121,16 @@ sudo xcodebuild -license accept # agree to xcode license
 
 # ides : vscode
 brew install --cask visual-studio-code
-mkdir -p ~/Library/Application\ Support/Code/User 
-rm ~/Library/Application\ Support/Code/User/settings.json 
-rm ~/Library/Application\ Support/Code/User/keybindings.json 
+mkdir -p ~/Library/Application\ Support/Code/User
+rm ~/Library/Application\ Support/Code/User/settings.json
+rm ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s ~/Home/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/Home/.vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 defaults write com.microsoft.VSCode.helper CGFontRenderingFontSmoothingDisabled -bool NO
 sudo /usr/sbin/DevToolsSecurity --enable
 
 # build tools
-brew install upx
+brew install upx   # the "Ultimate Packer for eXecutables"
 
 # cloud service providers
 #brew install heroku-toolbelt
@@ -140,23 +140,27 @@ brew install awscli
 brew install --cask docker virtualbox
 brew install podman
 
-# k8s 
+# k8s
 brew install kubernetes-helm terraform cfssl stern jsonnet openshift-cli
 
 # format utils : yaml json
-pip3 install yq
+pip3 install jq yq jid  # jsonquery yamlquery and json incremental digger
 go install github.com/brancz/gojsontoyaml@latest
-brew install jq jp jid
 
-# protocol clients : http
+# data visualization
+brew install jp # terminal plots: https://github.com/sgreben/jp
+
+# http dev
 brew install httpie
-# protocol clients : ws
-go install github.com/hashrocket/ws@latest
-# protocol client : TCP/UDP
-brew install netcat
-# protocol clients : dns
-brew install bind
 
+# websockets dev
+go install github.com/hashrocket/ws@latest
+
+# tcp/udp dev
+brew install netcat
+
+# dns dev
+brew install bind dig
 
 # disk maintenance
 brew install --cask google-drive mountain-duck grandperspective disk-inventory-x daisydisk grandperspective ipartition kekka
@@ -169,7 +173,7 @@ brew install watch fswatch
 brew install --cask iterm2
 # curl "https://raw.githubusercontent.com/nathanbuchar/atom-one-dark-terminal/master/scheme/iterm/One%20Dark.itermcolors" > ~/Downloads/OneDark.itermcolors
 
-# shell : zsh and fish 
+# shell : zsh and fish
 brew install zsh fish fisher
 echo Make sure you do the followin...
 echo sudo su -
@@ -181,7 +185,7 @@ echo chsh -s /usr/local/bin/fish jkassis
 # shell : directory environments
 brew install direnv
 
-# dbs 
+# dbs
 # brew install elasticsearch@5.6
 #brew services start elasticsearch@5.6
 # brew install cockroach
@@ -204,10 +208,10 @@ go install github.com/maykonlf/semver-cli/cmd/semver@latest
 #    brew tap sambadevi/powerlevel9k
 #    brew install powerlevel9k
 # Because these have PowerLine Icons...
-# https://github.com/Homebrew/homebrew-cask-fonts 
+# https://github.com/Homebrew/homebrew-cask-fonts
 brew tap caskroom/fonts
 brew tap homebrew/cask-fonts
-brew install --cask font-source-code-pro font-fira-code-nerd-font font-fira-mono-nerd-font font-hack-nerd-font 
+brew install --cask font-source-code-pro font-fira-code-nerd-font font-fira-mono-nerd-font font-hack-nerd-font
 # Apple has disabled Subpixel antialiasing for text in macOS Mojave. Re-enable with
 defaults write -g CGFontRenderingFontSmoothingDisabled -bool FALSE
 # undo that with
@@ -217,7 +221,7 @@ defaults write -g CGFontRenderingFontSmoothingDisabled -bool FALSE
 # system resource monitoring
 brew install little-snitch iperf htop
 
-# iot 
+# iot
 brew install balena-cli balenaetcher arduino teensy
 
 echo "Post installation tips:"
@@ -232,7 +236,7 @@ brew install tradingview
 brew install slic3r openscad ultimaker-cura autodesk-fusion360
 
 # network tools
-brew install nmap openvpn-connect angry-ip-scanner
+brew install nmap openvpn-connect angry-ip-scanner nmap
 
 # adobe
 brew install adobe-creative-cloud
