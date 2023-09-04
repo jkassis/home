61419f7ad6189057646021b0f3f7ebbd848429ba
Jeremy Kassis <jkassis@gmail.com>
2023-09-04 08:12:36 -0700

    adjust for gitconfig.local

--- bin/installZsh.sh
+++ bin/installZsh.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-# make directories
-mkdir "$HOME/zsh"
+# init the local file
 touch "$HOME/.zshrc.local"
 
-# install autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/plugins/zsh-autosuggestions"
-
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# install powerlevel9k theme
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-# change shell to zsh
-chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
